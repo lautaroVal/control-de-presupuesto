1ecc9afa50d5dfacc57b737ee6b92c0cd1e69ead
lautaroVal <valenzuelalautaro@yahoo.com>
2023-05-07 10:59:54 -0300

    Calculo de dispinible y gastado del presupuesto

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-const ControlPresupuesto = ({ presupuesto }) => {
+const ControlPresupuesto = ({ gastos, presupuesto }) => {
+
+  const [disponible, setDisponible] = useState(0)
+  const [gastado, setGastado] = useState(0)
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce( (total, gastos) => gastos.cantidad + total, 0)
+
+    const totalDisponible = presupuesto - totalGastado
+
+    console.log(totalDisponible)
+
+    setDisponible(totalDisponible)
+    setGastado(totalGastado)
+  }, [gastos])
+
   const formatearCantidad = (cantidad) => {
     return cantidad.toLocaleString("en-US", { 
         style: "currency", 
@@ -20,11 +35,11 @@ const ControlPresupuesto = ({ presupuesto }) => {
         </p>
 
         <p>
-          <span>Disponible: </span> {formatearCantidad(0)}
+          <span>Disponible: </span> {formatearCantidad(disponible)}
         </p>
 
         <p>
-          <span>Gastado: </span> {formatearCantidad(0)}
+          <span>Gastado: </span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,34 @@
 import {useState} from 'react'
+import Mensaje from './Mensaje'
 import CerrarBtn from '../img/cerrar.svg'
 
-const Modal = ({setModal, animarModal, setAnimarModal}) => {
+const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
 
+    const [mensaje, setMensaje] = useState('')    
     const [nombre, setNombre] = useState('')
-    const [cantidad, setCantidad] = useState(0)    
+    const [cantidad, setCantidad] = useState('')    
     const [categoria, setCategoria] = useState('')    
 
     const ocultarModal = () => {
         setAnimarModal(false)
-
         setTimeout(() => {
             setModal(false)
         }, 500)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if ([nombre, cantidad, categoria].includes('')) {
+            setMensaje('Todos los campos on obligtorios')
+
+            setTimeout(() => {
+                setMensaje('')
+            }, 3000);
+            return 
+        }
+
+        guardarGasto({nombre, cantidad, categoria})
     }
 
   return (
@@ -25,8 +41,12 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             />
         </div>
 
-        <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
+        <form 
+        onSubmit={handleSubmit}
+        className={`formulario ${animarModal ? "animar" : 'cerrar'}`}
+        >
             <legend>Nuevo Gasto</legend>
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
 
             <div className='campo'>
                 <label htmlFor="nombre">Nombre Gasto</label>
