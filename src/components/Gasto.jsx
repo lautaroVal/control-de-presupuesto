4bb176f115d497ff65f9b6f1b7a29a8617e1d388
lautaroVal <valenzuelalautaro@yahoo.com>
2023-05-31 11:42:48 -0300

    colocando el gasto a editar en el state y en el formulario

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,5 +1,15 @@
-import React from "react";
-import { formatearFecha } from "../helpers";
+import React from "react"
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions
+} from 'react-swipeable-list'
+import "react-swipeable-list/dist/styles.css"
+
+import { formatearFecha } from "../helpers"
+
 import IconoAhorro from '../img/icono_ahorro.svg'
 import IconoCasa from '../img/icono_casa.svg'
 import IconoComida from '../img/icono_comida.svg'
@@ -18,9 +28,31 @@ const diccionarioIconos = {
     suscripciones: IconoSuscripciones
 }
 
-const Gasto = ({ gasto }) => {
+const Gasto = ({ gasto, setGastoEditar }) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto;
+  
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={() => setGastoEditar(gasto)}>
+        Editar
+      </SwipeAction>
+    </LeadingActions>
+  )
+   
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction onClick={() => console.log('Eliminar')}>
+        Eliminar
+      </SwipeAction>
+    </TrailingActions>
+  )
+
   return (
+    <SwipeableList>
+      <SwipeableListItem
+      leadingActions={leadingActions()}
+      trailingActions={trailingActions()}
+      >
     <div className="gasto sombra">
       <div className="contenido-gasto">
         <img 
@@ -39,6 +71,8 @@ const Gasto = ({ gasto }) => {
 
       <p className="cantidad-gasto">${cantidad}</p>
     </div>
+    </SwipeableListItem>
+    </SwipeableList>
   );
 };
 
